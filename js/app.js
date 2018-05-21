9ee915bf663b9161c1abdd325a3d3de3e1e8ac46
Matt Diamond <mattdiamond80@gmail.com>
2018-05-20 21:41:31 -0500

    Add congratulations modal for a winning game

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@
 
 
 // 1. create an array that holds all of your cards
+const modal = document.querySelector('.modal');
 const stars = document.querySelector('.stars');
 const resetButton = document.querySelector('.restart');
 const counter = document.querySelector('.moves');
@@ -166,7 +167,11 @@ function enableCards() {
 function countMatches() {
   if (matches.length === 16) {
     console.log('you win!');
-    // const modal
+    modal.style.opacity = 1;
+    modal.style.display = 'block';
+    const modalContent = document.querySelector('.modal-content');
+    modalContent.classList.add('bounceInDown', 'animated');
+    // remove bounce and animated classes?
   }
 }
 
@@ -186,15 +191,3 @@ function shuffle(array) {
 
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
